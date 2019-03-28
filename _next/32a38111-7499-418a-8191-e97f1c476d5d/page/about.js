af16c710feb5bc9e697ab742902cc2c12a6bad6b
jirik <jirik@users.noreply.github.com>
2019-03-28 15:34:03 +0100

    Publish

--- _next/32a38111-7499-418a-8191-e97f1c476d5d/page/about.js
+++
@@ -1,1 +0,0 @@
-module.exports=__NEXT_REGISTER_PAGE("/about",function(){return{page:webpackJsonp([5],{516:function(e,t,n){e.exports=n(517)},517:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var u=n(0),a=n.n(u),o=n(241);t.default=function(){return a.a.createElement(o.a,null,a.a.createElement("p",null,"This is the about page"))}}},[516]).default}});
